6785b305ed1f7b47f71cc0bbd62ed9e9ebfc16ff
Aleytia <jarek13705@gmail.com>
2020-02-13 01:17:17 -0800

    Update some links

--- routes/home.js
+++ routes/home.js
@@ -6,7 +6,6 @@ var data = require("../data/data.json")
 
 exports.view = function(req, res) {
     const restaurants = Object.keys(data)
-    console.log(restaurants)
     res.render('home', {
         restaurants: restaurants
     });
--- routes/menu.js
+++ routes/menu.js
@@ -1,4 +1,14 @@
+var data = require("../data/data.json");
 
 exports.view = function(req, res) {
-    res.render('menu')
+
+    var restaurant = req.query.restaurant;
+    var course = req.query.course ? req.query.course : "breakfast";
+
+    var pagedata = {
+        "name": restaurant,
+        "food": data[restaurant][course]
+    }
+
+    res.render('menu', pagedata)
 }
