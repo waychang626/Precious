bbc7b8ee56512decaeef3a75dc7031ce4450d05c
Aleytia <jarek13705@gmail.com>
2020-02-15 03:48:51 -0800

    animations adfasdf

---
+++ public/js/home.js
@@ -0,0 +1,63 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function () {
+    initializePage();
+    $("#all").click(swapToAll);
+    $("#dining-hall").click(swapToDining);
+    $("#cafe").click(swapToCafes);
+    $("#markets").click(swapToMarkets);
+});
+
+function initializePage() {
+    // add any functionality and listeners you want here
+    console.log("JavaScript initialized on Home page.");
+}
+
+function swapToAll(e) {
+    e.preventDefault();
+    clearSelected();
+    $("#all-text").addClass("selected");   
+    addHide();
+    $("#all-list").removeClass("hide");
+}
+
+function swapToDining(e) {
+    e.preventDefault();
+    clearSelected();
+    $("#dining-hall-text").addClass("selected");
+    addHide();
+    $("#dining-halls-list").removeClass("hide");
+}
+
+function swapToCafes(e) {
+    e.preventDefault();
+    clearSelected();
+    $("#cafe-text").addClass("selected");
+    addHide();
+    $("#cafes-list").removeClass("hide");
+}
+
+function swapToMarkets(e) {
+    e.preventDefault();
+    clearSelected();
+    $("#markets-text").addClass("selected");
+    addHide();
+    $("#markets-list").removeClass("hide")
+}
+
+// Helper to clear the selected text
+function clearSelected() {
+    $("#all-text").removeClass("selected");
+    $("#dining-hall-text").removeClass("selected");
+    $("#cafe-text").removeClass("selected");
+    $("markets-text").removeClass("selected");
+}
+
+// Helper to add hide to every div
+function addHide() {
+    $("#all-list").addClass("hide");
+    $("#dining-halls-list").addClass("hide");
+    $("#cafes-list").addClass("hide");
+    $("#markets-list").addClass("hide");
+}
--- routes/home.js
+++ routes/home.js
@@ -4,11 +4,40 @@
 
 var data = require("../data/data.json")
 
-exports.view = function(req, res) {
-    var restaurants = Object.keys(data)
-    restaurants.sort();
+exports.view = function (req, res) {
+
+    entries = Object.entries(data);
+
+    all = [];
+    dining_halls = [];
+    cafes = [];
+    markets = [];
+
+    for (i in entries) {
+        name = entries[i][0];
+        info = entries[i][1];
+        eatery_type = info['eatery_type']
+
+        all.push(name);
+        
+        if (eatery_type == 1) {
+            dining_halls.push(name);
+        } else if (eatery_type == 2) {
+            cafes.push(name);
+        } else if (eatery_Type == 3) {
+            markets.push(name);
+        }
+    }
+
+    all.sort();
+    dining_halls.sort();
+    cafes.sort();
+    markets.sort();
 
     res.render('home', {
-        restaurants: restaurants
+        "all": all,
+        "dining_halls": dining_halls,
+        "cafes": cafes,
+        "markets": markets
     });
 };
