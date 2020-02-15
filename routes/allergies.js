dd2b9182c6cc43d88ee7dec42f3a455df7ccff6e
Aleytia <jarek13705@gmail.com>
2020-02-14 19:28:38 -0800

    Minor redesign updates

--- routes/allergies.js
+++ routes/allergies.js
@@ -2,6 +2,12 @@
  * Allergies page
  */
 
+var allergies = require("../data/allergies.json")
+
+allergies.sort(); // Sort data to make it easier to find
+
 exports.view = function(req, res) {
-    res.render("allergies", {});
+    res.render("allergies", {
+        "allergies": allergies
+    });
 }
